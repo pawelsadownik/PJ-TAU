8f3ed63f2e17b4675a71b7a9566d4a65240107b9
psadownik <p.sadownik@npc.pl>
2019-11-25 23:22:40 +0100

    code clean

--- src/main/java/pl/edu/pjatk/tau/Repository/TrainingDetailsRepository.java
+++ src/main/java/pl/edu/pjatk/tau/Repository/TrainingDetailsRepository.java
@@ -9,12 +9,6 @@ import java.util.List;
 
 @Repository
 public interface TrainingDetailsRepository extends JpaRepository<TrainingDetails, Integer> {
-/*
-    List<TrainingDetails> getAllTrainings();
-    TrainingDetails getTrainingDetailsById(int id);
-    int addTrainingDetails(TrainingDetails trainingDetails);
-    int removeTrainingDetails(int id);
-    int updateTrainingDetails(TrainingDetails trainingDetails);
-*/
+
 }
 
